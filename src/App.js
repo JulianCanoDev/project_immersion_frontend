31590e6718fc1d07957de0c37050a8fea3b72e1e
julian <juliancano.dev@gmail.com>
2020-09-19 15:05:45 -0500

    Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
+import { About } from './components/About';
+import { Users } from './components/Users';
+import { Navbar } from './components/Navbar';
+import { Register } from './components/Register';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <div>
+        <Switch>
+          <Route path="/" component={Users} />
+          <Route path="/about" component={About} />
+          <Route path="/register" component={Register} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
